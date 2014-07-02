31e38d088d9231cbf6e011e4be8ef5af866a3d85
Phill <phillclarke@gmail.com>
2014-07-02 17:19:43 +0100

    Update ui.R

--- LongestLosing/ui.R
+++ LongestLosing/ui.R
@@ -7,7 +7,7 @@
 # Longest Losing Sequence and the LonhRo apps are distributed in the hope that they will be useful, but WITHOUT ANY WARRANTY; without even 
 # the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with Longest Losing Sequence.  If not, see <http://www.gnu.org/licenses/>.
 
 # All LonhRo apps can be found in the following GitHub repository: https://github.com/phillc73/lonhRo
 # Each app is a new branch of that repository
